60f1be45e43819023fadef0e826b8d5a84670e1c
Mateus Paiva <mateusopaiva@gmail.com>
2022-07-25 14:39:53 -0300

    Personalizando o Campo de Texto

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -5,8 +5,31 @@ class TodoListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return const Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: 'Preço',
+              hintText: 'exemplo@exemplo.com',
+              //border: InputBorder.none,
+              errorText: null,
+              prefixText: 'R\$ ',
+              suffixText: 'cm',
+              labelStyle: TextStyle(
+                fontSize: 40,
+              ),
+            ),
+            //obscureText: true,
+            keyboardType: TextInputType.number,
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              color: Colors.purple,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
