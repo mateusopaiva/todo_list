870c7e17d78ea404367360adb377e736589d9d81
Mateus Paiva <mateusopaiva@gmail.com>
2022-08-18 15:33:47 -0300

    Carregando a Lista de Tarefas

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -20,6 +20,17 @@ class _TodoListPageState extends State<TodoListPage> {
   int? deletedTodoPos;
 
   @override
+  void initState() {
+    super.initState();
+
+    todoRepository.getTodoList().then((value) {
+      setState(() {
+        todos = value;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
@@ -115,6 +126,7 @@ class _TodoListPageState extends State<TodoListPage> {
     setState(() {
       todos.remove(todo);
     });
+    todoRepository.saveTodoList(todos);
 
     ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
@@ -130,6 +142,7 @@ class _TodoListPageState extends State<TodoListPage> {
           setState(() {
             todos.insert(deletedTodoPos!, deletedTodo!);
           });
+          todoRepository.saveTodoList(todos);
         },
       ),
       duration: const Duration(seconds: 5),
